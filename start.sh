7362f793672fe651a35ab2d7d04b207fa09fac34
Lukas Fruntke <lukas@fruntke.tech>
2024-04-03 15:36:02 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,35 +1,3 @@
 #!/bin/bash
 
-# Function to start Flask app
-start_flask() {
-    flask run --host=0.0.0.0 --port=8082 &
-    FLASK_PID=$!
-}
-
-# Function to start mitmdump
-start_mitmdump() {
-    mitmdump --set confdir=/home/mitmproxy/.mitmproxy -s /home/mitmproxy/mitmproxy_addon.py -p 8080 &
-    MITMDUMP_PID=$!
-}
-
-cleanup() {
-    echo "Signal received, shutting down gracefully..."
-    # Kill Flask and mitmdump if they are running
-    [[ -n $FLASK_PID ]] && kill $FLASK_PID
-    [[ -n $MITMDUMP_PID ]] && kill $MITMDUMP_PID
-    # Wait for Flask and mitmdump to terminate
-    wait $FLASK_PID
-    wait $MITMDUMP_PID
-    exit 0
-}
-
-# Trap signals
-trap cleanup SIGTERM SIGINT
-
-# Start Flask and mitmdump
-start_flask
-start_mitmdump
-
-# Wait for Flask and mitmdump to finish
-wait $FLASK_PID
-wait $MITMDUMP_PID
+mitmdump --set confdir=~/.mitmproxy -s mitmproxy_addon.py -p 8080 --set validate_inbound_headers=false --no-http2
