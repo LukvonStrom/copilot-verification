c499b85d8b6f25a33e8559a120b126bcfcd1f76b
Lukas Fruntke <lukas@fruntke.tech>
2024-04-04 18:03:34 +0100

    add first setup

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mitmdump --set confdir=~/.mitmproxy -s mitmproxy_addon.py -p 8080 --set validate_inbound_headers=false --no-http2
+cd /home/mitmproxy/
+ls .
+mitmdump --set confdir=~/.mitmproxy -s /home/mitmproxy/mitmproxy_addon.py -p 8080 --set validate_inbound_headers=false --no-http2 
+
+
